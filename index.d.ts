1175d3b9fa4a9d12252e9771ea82db01d8997890
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-11-13 19:51:08 -0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
 export function copyDir(sourceDirectory: string, targetDirectory: string): void
 export function copyDirAsync(sourceDirectory: string, targetDirectory: string, callback?: (err: NodeJS.ErrnoException) => void): void
+export function copyDirContent(sourceDirectory: string, targetDirectory: string, ignoreExtensions?: string[]): void
+export function copyFile(filepath: string, targetDirectory: string): void
